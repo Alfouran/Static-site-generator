20dc6c8d846ca39cf9c87c90c2d5fcf3e29c6eff
Alfouran <scrivner.patrick@gmail.com>
2024-04-18 23:29:01 -0500

    added child class to HTMLNode and added testing

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -11,10 +11,21 @@ class HTMLNode:
     def props_to_html(self):
         key_value = ""
         for key, value in self.props.items():
-            key_value += f" {key}={value}"
+            key_value += f' {key}="{value}"'
         return key_value
     
     def __repr__(self):
         return f"{self.tag}, {self.value}, {self.children}, {self.props}"
         
+class LeafNode(HTMLNode):
+    def __init__(self, tag=None, value=None, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value == None:
+            raise ValueError("Value cannot be None")
+        if self.tag == None:
+            return f"{self.value}"
+        attrs_str = self.props_to_html()
+        return f"<{self.tag}{attrs_str}>{self.value}</{self.tag}>"
 
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,12 +1,12 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html(self):
         node = HTMLNode(None, None, None, {"href": "https://www.google.com", "target": "_blank"})
-        expected_output = " href=https://www.google.com target=_blank"
+        expected_output = ' href="https://www.google.com" target="_blank"'
         self.assertEqual(node.props_to_html(), expected_output)
 
     def test_repr(self):
@@ -17,6 +17,14 @@ class TestHTMLNode(unittest.TestCase):
                         )
         expected_result = "p, This is text in a paragraph, ['child1', 'child2'], {'href': 'https://www.google.com', 'target': '_blank'}"
         self.assertEqual(repr(node), expected_result)
+
+    def test_to_html(self):
+        node = LeafNode("p", 
+            "This is a paragraph of text.",
+            {})
+        expected_result = "<p>This is a paragraph of text.</p>"
+        self.assertEqual(node.to_html(), expected_result)
+        
 
 if __name__ == "__main__":
     unittest.main()
